168eb47b544934de2db15bf35907929987400f50
Paul Cohn <pscohn@gmail.com>
2015-07-07 00:14:09 +0000

    Clarify indexes

--- algorithms/test_union.h
+++ algorithms/test_union.h
@@ -5,28 +5,28 @@
 
 void test_quickfind()
 {
-    int numbers[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int numbers[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-    quickfind_union(&numbers[0], 8, 3, 5); // sets index 5 to index 3's value
-    int correct[8] = {1, 2, 3, 4, 5, 4, 7, 8};
-    assert(check_arr_equal(&numbers[0], &correct[0], 8));
-    assert(quickfind_find(&numbers[0], 3, 5));
+    quickfind_union(&numbers[0], 9, 4, 6); // sets index 5 to index 3's value
+    int correct[9] = {0, 1, 2, 3, 4, 5, 4, 7, 8};
+    assert(check_arr_equal(&numbers[0], &correct[0], 9));
+    assert(quickfind_find(&numbers[0], 4, 6));
 
-    quickfind_union(&numbers[0], 8, 6, 7);
-    int correct2[8] = {1, 2, 3, 4, 5, 4, 7, 7};
-    assert(check_arr_equal(&numbers[0], &correct2[0], 8));
-    assert(quickfind_find(&numbers[0], 3, 5));
+    quickfind_union(&numbers[0], 9, 7, 8);
+    int correct2[9] = {0, 1, 2, 3, 4, 5, 4, 7, 7};
+    assert(check_arr_equal(&numbers[0], &correct2[0], 9));
+    assert(quickfind_find(&numbers[0], 4, 6));
+    assert(quickfind_find(&numbers[0], 7, 8));
+
+    quickfind_union(&numbers[0], 9, 4, 8);
+    int correct3[9] = {0, 1, 2, 3, 4, 5, 4, 4, 4};
+    assert(check_arr_equal(&numbers[0], &correct3[0], 9));
+    assert(quickfind_find(&numbers[0], 4, 6));
+    assert(quickfind_find(&numbers[0], 4, 7));
+    assert(quickfind_find(&numbers[0], 4, 8));
     assert(quickfind_find(&numbers[0], 6, 7));
-
-    quickfind_union(&numbers[0], 8, 3, 7);
-    int correct3[8] = {1, 2, 3, 4, 5, 4, 4, 4};
-    assert(check_arr_equal(&numbers[0], &correct3[0], 8));
-    assert(quickfind_find(&numbers[0], 3, 5));
-    assert(quickfind_find(&numbers[0], 3, 6));
-    assert(quickfind_find(&numbers[0], 3, 7));
-    assert(quickfind_find(&numbers[0], 5, 6));
-    assert(quickfind_find(&numbers[0], 5, 7));
-    assert(quickfind_find(&numbers[0], 6, 7));
+    assert(quickfind_find(&numbers[0], 6, 8));
+    assert(quickfind_find(&numbers[0], 7, 8));
 }
 
 void test_union()
