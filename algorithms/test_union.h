0fdaff5c0df6adf06c9e45ce37b63eb0858fae82
Paul Cohn <pscohn@gmail.com>
2015-07-07 00:08:26 +0000

    Rename quickmind functions

--- algorithms/test_union.h
+++ algorithms/test_union.h
@@ -7,26 +7,26 @@ void test_quickfind()
 {
     int numbers[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-    union_join(&numbers[0], 8, 3, 5); // sets index 5 to index 3's value
+    quickfind_union(&numbers[0], 8, 3, 5); // sets index 5 to index 3's value
     int correct[8] = {1, 2, 3, 4, 5, 4, 7, 8};
     assert(check_arr_equal(&numbers[0], &correct[0], 8));
-    assert(union_find(&numbers[0], 3, 5));
+    assert(quickfind_find(&numbers[0], 3, 5));
 
-    union_join(&numbers[0], 8, 6, 7);
+    quickfind_union(&numbers[0], 8, 6, 7);
     int correct2[8] = {1, 2, 3, 4, 5, 4, 7, 7};
     assert(check_arr_equal(&numbers[0], &correct2[0], 8));
-    assert(union_find(&numbers[0], 3, 5));
-    assert(union_find(&numbers[0], 6, 7));
+    assert(quickfind_find(&numbers[0], 3, 5));
+    assert(quickfind_find(&numbers[0], 6, 7));
 
-    union_join(&numbers[0], 8, 3, 7);
+    quickfind_union(&numbers[0], 8, 3, 7);
     int correct3[8] = {1, 2, 3, 4, 5, 4, 4, 4};
     assert(check_arr_equal(&numbers[0], &correct3[0], 8));
-    assert(union_find(&numbers[0], 3, 5));
-    assert(union_find(&numbers[0], 3, 6));
-    assert(union_find(&numbers[0], 3, 7));
-    assert(union_find(&numbers[0], 5, 6));
-    assert(union_find(&numbers[0], 5, 7));
-    assert(union_find(&numbers[0], 6, 7));
+    assert(quickfind_find(&numbers[0], 3, 5));
+    assert(quickfind_find(&numbers[0], 3, 6));
+    assert(quickfind_find(&numbers[0], 3, 7));
+    assert(quickfind_find(&numbers[0], 5, 6));
+    assert(quickfind_find(&numbers[0], 5, 7));
+    assert(quickfind_find(&numbers[0], 6, 7));
 }
 
 void test_union()
--- algorithms/union.h
+++ algorithms/union.h
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 
-void union_join(int* numbers, int len, int p, int q)
+/*
+ * Quickfind Implementation
+ */
+
+void quickfind_union(int* numbers, int len, int p, int q)
 {
     int old_val = numbers[q];
     int new_val = numbers[p];
@@ -13,7 +17,7 @@ void union_join(int* numbers, int len, int p, int q)
     }
 }
 
-int union_find(int* numbers, int p, int q)
+int quickfind_find(int* numbers, int p, int q)
 {
     if (numbers[p] == numbers[q]) {
         return 1;
